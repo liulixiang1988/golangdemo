62681a9dfefa9e0e65edcb0cc946eccfd5150b36
Liu Lixiang <liulixiang1988@gmail.com>
2019-07-27 07:55:48 +0800

    select & timeout

--- concurrency/gocurr2_test.go
+++ concurrency/gocurr2_test.go
@@ -1,8 +1,29 @@
 package concurrency
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func AsyncTimeDelay(timeDelay time.Duration) chan string {
+	var retCh = make(chan string)
+	go func() {
+		fmt.Println("start")
+		time.Sleep(timeDelay * time.Millisecond)
+		retCh <- "ok"
+		fmt.Println("done")
+	}()
+	return retCh
+}
 
 // TestSelectAndTimeOut 多路选择与超时
 func TestSelectAndTimeOut(t *testing.T) {
-
+	retCh := AsyncTimeDelay(100)
+	select {
+	case v, r := <-retCh:
+		fmt.Println("v:", v, " r:", r)
+	case <-time.After(time.Millisecond * 50):
+		fmt.Println("timeout")
+	}
 }
